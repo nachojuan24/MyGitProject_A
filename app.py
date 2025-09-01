c6f9c5bca95fc432eabeb244e1c2339060a99207
DESKTOP-DTCV25D\Nacho <borderai@uni-mainz.de>
2025-09-01 17:30:35 +0200

    Variables en ruta

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return '<h1> Hello World! </h1>'
 
 
-@app.route('/<name>')
-def print_name(name):
-    return 'Welcome back, {}'.format(name)
+@app.route('/hello')
+@app.route('/hello/<name>')
+@app.route('/hello/<name>/<int:age>')
+def hello(name = None, age=None):
+    if name==None and age==None:
+        return '<h2> Hello world! </h2>'
+    elif age == None:
+        return f'<h2> Hello {name}! </h2>'
+    else:
+        return f'<h2> Hello {name}! Two times your age is {age*2}, right? </h2>'
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run()
+    app.run(debug=True)
+    # app.run()
