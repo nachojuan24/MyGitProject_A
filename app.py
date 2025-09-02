3e2f1b04954f4488279d6f125cdfa321c15d5534
DESKTOP-DTCV25D\Nacho <borderai@uni-mainz.de>
2025-09-02 09:16:20 +0200

    Enviar Datos A Plantilla

--- app.py
+++ app.py
@@ -34,13 +34,14 @@ def index():
 @app.route('/hello')
 @app.route('/hello/<name>')
 @app.route('/hello/<name>/<int:age>')
-def hello(name = None, age=None):
-    if name==None and age==None:
-        return '<h2> Hello world! </h2>'
-    elif age == None:
-        return f'<h2> Hello {name}! </h2>'
-    else:
-        return f'<h2> Hello {name}! Two times your age is {age*2}, right? </h2>'
+@app.route('/hello/<name>/<int:age>/<email>')
+def hello(name = None, age=None, email=None):
+    my_data = {
+        'name': name, 
+        'age': age, 
+        'email': email
+    }
+    return render_template('hello.html', data = my_data)
 
 
 @app.route('/code/<path:code>')
