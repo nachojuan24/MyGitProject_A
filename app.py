2164691caca9ca26dbce225f9521947aa3117363
DESKTOP-DTCV25D\Nacho <borderai@uni-mainz.de>
2025-09-02 19:49:16 +0200

    Formulario con flask_wtf

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from markupsafe import escape
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY = 'dev'
+)
 
 #Filters
 @app.add_template_filter
@@ -55,20 +59,35 @@ def hello(name = None, age=None, email=None):
 def code(code):
     return f'<code>{escape(code)}</code>'
 
+#Create a form with wtform
+class ResgisterForm(FlaskForm):
+    username = StringField("Username: ", validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField("Password: ", validators=[DataRequired(), Length(min=6, max=40)])
+    submit = SubmitField("Sign up ")
+
 # Register user
 @app.route('/auth/register', methods = ['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    form = ResgisterForm()
+    
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        return f"Username: {username}, Password: {password}"
 
-        if len(username) >= 4 and len(username) <= 25 and len(password) >= 6 and len(password)<=40:
-            return f"Username: {username}, Password: {password}"
-        else:
-            error = """The username must have between 4 and 25 characters. 
-            The password must also have between 6 and 40 characters."""
-            return render_template('auth/register.html', error = error)
-    return render_template('auth/register.html')
+    # if request.method == 'POST':
+    #     username = request.form['username']
+    #     password = request.form['password']
+
+    #     if len(username) >= 4 and len(username) <= 25 and len(password) >= 6 and len(password)<=40:
+    #         return f"Username: {username}, Password: {password}"
+    #     else:
+    #         error = """The username must have between 4 and 25 characters. 
+    #         The password must also have between 6 and 40 characters."""
+
+    #         return render_template('auth/register.html', form=form, error = error)
+        
+    return render_template('auth/register.html', form=form)
 
 
 if __name__ == '__main__':
